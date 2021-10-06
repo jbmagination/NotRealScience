796b01ddfd17ff19c2aec38452523887cddf7e73
jbmagination <26027089+jbmagination@users.noreply.github.com>
2021-10-05 21:34:39 -0400

    Plugin commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NotRealScience
+Blocks the `/science` and `/track` endpoints. Pretty simple plugin, verify yourself that it works with DevTools.
+
+## Installing
+Assuming you've installed Powercord (learn more [here](https://powercord.dev)):
+1. Clone this repo to your Powercord plugins folder (found at `powercord/src/Powercord/plugins`)
+2. Refresh Discord Canary (CTRL+R)
---
+++ index.js
@@ -0,0 +1,24 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class NotRealScience extends Plugin {
+  startPlugin () {
+    require('electron').webFrame.executeJavaScript(`
+      xmlhttp = window.XMLHttpRequest.prototype.open;
+      (function (open) {
+        window.XMLHttpRequest.prototype.open = function (a, url, b, c, d) {
+          if (url.toString().includes('/science' || '/track')) {
+            return open.apply(this, false);
+          }
+          return open.apply(this, arguments);
+        };
+      }(XMLHttpRequest.prototype.open))`
+    );
+  }
+
+  pluginWillUnload () {
+    require('electron').webFrame.executeJavaScript(`
+      window.XMLHttpRequest.prototype.open = xmlhttp;
+      delete xmlhttp;
+  `);
+  }
+};
